58739655506dd873ede23ee8bc24375b1db3be9a
Manste <manuelle.ndamtang@yahoo.com>
2020-02-26 00:22:36 +0100

    Adding some configuration for compute1 and controller nodes

---
+++ Controller/controller.sh
@@ -0,0 +1,270 @@
+#*************************** controller*****************************
+
+#crudini va nous servir plutard pour la configuration de certains packages
+
+#Configurer la base de données SQL
+apt install mariadb-server python3-pymysql -y
+cat > /etc/mysql/mariadb.conf.d/99-openstack.cnf << EOT
+[mysqld]
+bind-address = 10.0.0.11
+
+default-storage-engine = innodb
+innodb_file_per_table = on
+max_connections = 4096
+collation-server = utf8_general_ci
+character-set-server = utf8
+EOT
+service mysql restart
+
+#Configurer la File d'attente de messages 
+apt install rabbitmq-server
+rabbitmqctl add_user openstack RABBIT_PASS
+rabbitmqctl set_permissions openstack ".*" ".*" ".*"
+
+#Configuration de Memcached
+apt install memcached python3-memcache -y
+#Allez dans le fichier /etc/memcached.conf et modifiez la ligne "-l 127.0.0.1" en "-l 10.0.0.11"
+#Ensuite redemarrez le service memcached "service memcached restart"
+
+#Configuration d'ETCD
+apt install etcd -y
+cat >> /etc/default/etcd << EOT
+ETCD_NAME="controller"
+ETCD_DATA_DIR="/var/lib/etcd"
+ETCD_INITIAL_CLUSTER_STATE="new"
+ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster-01"
+ETCD_INITIAL_CLUSTER="controller=http://10.0.0.11:2380"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="http://10.0.0.11:2380"
+ETCD_ADVERTISE_CLIENT_URLS="http://10.0.0.11:2379"
+ETCD_LISTEN_PEER_URLS="http://0.0.0.0:2380"
+ETCD_LISTEN_CLIENT_URLS="http://10.0.0.11:2379"
+EOT
+systemctl enable etcd
+systemctl restart etcd
+
+#Installation et Configuration de Keystone
+mysql -u root -p <<< "CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS';"
+apt install keystone libapache2-mod-wsgi -y
+crudini --set /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:KEYSTONE_DBPASS@controller/keystone
+crudini --set /etc/keystone/keystone.conf token provider fernet
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password ADMIN_PASS \
+  --bootstrap-admin-url http://controller:5000/v3/ \
+  --bootstrap-internal-url http://controller:5000/v3/ \
+  --bootstrap-public-url http://controller:5000/v3/ \
+  --bootstrap-region-id RegionOne
+echo "ServerName controller" >> /etc/apache2/apache2.conf
+service apache2 restart
+
+exit
+cat > admin-openrc << EOT
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=ADMIN_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOT
+
+openstack project create --domain default \
+  --description "Service Project" service
+  
+openstack project create --domain default \
+  --description "Demo Project" demo  
+  
+openstack user create --domain default \
+  --password "DEMO_PASS" demo
+
+openstack role create demorole
+
+openstack role add --project demo --user demo demorole 
+
+cat > demo-openrc << EOT
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=DEMO_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOT
+
+#********************************** Controller
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Installation et configuration de Glance %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mysql <<< "CREATE DATABASE glance;
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'GLANCE_DBPASS';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'GLANCE_DBPASS';"
+
+exit
+. admin-openrc
+
+openstack user create --domain default --password "GLANCE_DBPASS" glance
+openstack role add --project service --user glance admin
+
+openstack service create --name glance \
+  --description "OpenStack Image" image
+
+openstack endpoint create --region RegionOne \
+  image public http://controller:9292
+
+openstack endpoint create --region RegionOne \
+  image internal http://controller:9292
+
+openstack endpoint create --region RegionOne \
+  image admin http://controller:9292
+
+apt install glance -y
+
+crudini --set /etc/glance/glance-api.conf database connection mysql+pymysql://glance:GLANCE_DBPASS@controller/glance
+crudini --set /etc/glance/glance-api.conf keystone_authtoken www_authenticate_uri http://controller:5000
+crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_url http://controller:5000
+crudini --set /etc/glance/glance-api.conf keystone_authtoken memcached_servers controller:11211
+crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_type password
+crudini --set /etc/glance/glance-api.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/glance/glance-api.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/glance/glance-api.conf keystone_authtoken project_name service
+crudini --set /etc/glance/glance-api.conf keystone_authtoken username glance
+crudini --set /etc/glance/glance-api.conf keystone_authtoken password GLANCE_PASS
+crudini --set /etc/glance/glance-api.conf paste_deploy flavor keystone
+crudini --set /etc/glance/glance-api.conf glance_store stores "file,http"
+crudini --set /etc/glance/glance-api.conf glance_store default_store file
+crudini --set /etc/glance/glance-api.conf glance_store filesystem_store_datadir /var/lib/glance/images/
+
+su -s /bin/sh -c "glance-manage db_sync" glance
+service glance-api restart
+
+exit
+. admin-openrc
+wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+
+glance image-create --name "cirros" \
+  --file cirros-0.4.0-x86_64-disk.img \
+  --disk-format qcow2 --container-format bare \
+  --visibility public
+
+glance image-list
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Installation et configuration de Placement %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sudo su
+mysql <<< "CREATE DATABASE placement;
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'localhost' IDENTIFIED BY 'PLACEMENT_DBPASS';
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'%' IDENTIFIED BY 'PLACEMENT_DBPASS';"
+
+exit
+. admin-openrc
+openstack user create --domain default --password "PLACEMENT_DBPASS" placement
+openstack role add --project service --user placement admin
+
+openstack service create --name placement \
+  --description "Placement API" placement
+
+openstack endpoint create --region RegionOne \
+  placement public http://controller:8778
+  
+openstack endpoint create --region RegionOne \
+  placement internal http://controller:8778
+
+openstack endpoint create --region RegionOne \
+  placement admin http://controller:8778
+
+sudo su
+apt install placement-api python3-pip -y
+crudini --set /etc/placement/placement.conf placement_database connection mysql+pymysql://placement:PLACEMENT_DBPASS@controller/placement
+crudini --set /etc/placement/placement.conf api auth_strategy keystone
+crudini --set /etc/placement/placement.conf keystone_authtoken auth_url http://controller:5000/v3
+crudini --set /etc/placement/placement.conf keystone_authtoken memcached_servers controller:11211
+crudini --set /etc/placement/placement.conf keystone_authtoken auth_type password
+crudini --set /etc/placement/placement.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/placement/placement.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/placement/placement.conf keystone_authtoken project_name service
+crudini --set /etc/placement/placement.conf keystone_authtoken username placement
+crudini --set /etc/placement/placement.conf keystone_authtoken password PLACEMENT_PASS
+
+su -s /bin/sh -c "placement-manage db sync" placement
+service apache2 restart
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Installation et configuration de Nova %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sudo su
+mysql <<< "CREATE DATABASE nova_api;
+CREATE DATABASE nova;
+CREATE DATABASE nova_cell0;
+GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS';"
+
+exit
+. admin-openrc
+openstack user create --domain default --password "NOVA_DBPASS" nova
+
+openstack role add --project service --user nova admin
+
+openstack service create --name nova \
+  --description "OpenStack Compute" compute
+
+openstack endpoint create --region RegionOne \
+  compute public http://controller:8774/v2.1
+  
+openstack endpoint create --region RegionOne \
+  compute internal http://controller:8774/v2.1
+
+openstack endpoint create --region RegionOne \
+  compute admin http://controller:8774/v2.1
+  
+sudo su 
+apt install nova-api nova-conductor nova-novncproxy nova-scheduler -y
+
+crudini --set /etc/nova/nova.conf api_database connection mysql+pymysql://nova:NOVA_DBPASS@controller/nova_api
+crudini --set /etc/nova/nova.conf database connection mysql+pymysql://nova:NOVA_DBPASS@controller/nova
+crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:RABBIT_PASS@controller:5672/
+crudini --set /etc/nova/nova.conf api auth_strategy keystone
+crudini --set /etc/nova/nova.conf keystone_authtoken www_authenticate_uri http://controller:5000/
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:5000/
+crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers controller:11211
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_type password
+crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
+crudini --set /etc/nova/nova.conf keystone_authtoken username nova
+crudini --set /etc/nova/nova.conf keystone_authtoken password NOVA_PASS
+crudini --set /etc/nova/nova.conf DEFAULT my_ip 10.0.0.11
+crudini --set /etc/nova/nova.conf DEFAULT use_neutron true
+crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
+crudini --set /etc/nova/nova.conf vnc enabled true
+crudini --set /etc/nova/nova.conf vnc server_listen 10.0.0.11
+crudini --set /etc/nova/nova.conf vnc server_proxyclient_address 10.0.0.11
+crudini --set /etc/nova/nova.conf glance api_servers http://controller:9292
+crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
+crudini --set /etc/nova/nova.conf placement region_name RegionOne
+crudini --set /etc/nova/nova.conf placement project_domain_name Default
+crudini --set /etc/nova/nova.conf placement project_name service
+crudini --set /etc/nova/nova.conf placement auth_type password
+crudini --set /etc/nova/nova.conf placement user_domain_name Default
+crudini --set /etc/nova/nova.conf placement auth_url http://controller:5000/v3
+crudini --set /etc/nova/nova.conf placement username placement
+crudini --set /etc/nova/nova.conf placement password PLACEMENT_PASS
+
+su -s /bin/sh -c "nova-manage api_db sync" nova
+su -s /bin/sh -c "nova-manage cell_v2 map_cell0" nova
+su -s /bin/sh -c "nova-manage cell_v2 create_cell --name=cell1 --verbose" nova
+su -s /bin/sh -c "nova-manage db sync" nova
+su -s /bin/sh -c "nova-manage cell_v2 list_cells" nova
+
+service nova-api restart
+service nova-scheduler restart
+service nova-conductor restart
+service nova-novncproxy restart
+# Lancer la configuration du Compute1
