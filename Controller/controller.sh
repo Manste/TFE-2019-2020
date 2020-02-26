36819eac4a8e18d419f287ff4957abd7557188cc
Manste <manuelle.ndamtang@yahoo.com>
2020-02-26 23:38:30 +0100

    memcached, etcd

--- Controller/controller.sh
+++ Controller/controller.sh
@@ -1,5 +1,7 @@
-crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:openstack@controller
-crudini --set /etc/nova/nova.conf auth_strategy keystone
+crudini --set /etc/nova/nova.conf api_database connection mysql+pymysql://nova:openstack@controller/nova_api
+crudini --set /etc/nova/nova.conf database connection mysql+pymysql://nova:openstack@controller/nova
+crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:openstack@controller:5672/
+crudini --set /etc/nova/nova.conf api auth_strategy keystone
 crudini --set /etc/nova/nova.conf keystone_authtoken www_authenticate_uri http://controller:5000/
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:5000/
 crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers controller:11211
@@ -9,13 +11,12 @@ crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name Default
 crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
 crudini --set /etc/nova/nova.conf keystone_authtoken username nova
 crudini --set /etc/nova/nova.conf keystone_authtoken password openstack
-crudini --set /etc/nova/nova.conf DEFAULT my_ip 10.0.0.31
+crudini --set /etc/nova/nova.conf DEFAULT my_ip 10.0.0.11
 crudini --set /etc/nova/nova.conf DEFAULT use_neutron true
 crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
 crudini --set /etc/nova/nova.conf vnc enabled true
-crudini --set /etc/nova/nova.conf vnc server_listen 0.0.0.0
-crudini --set /etc/nova/nova.conf vnc server_proxyclient_address 10.0.0.31
-crudini --set /etc/nova/nova.conf vnc novncproxy_base_url http://controller:6080/vnc_auto.html
+crudini --set /etc/nova/nova.conf vnc server_listen 10.0.0.11
+crudini --set /etc/nova/nova.conf vnc server_proxyclient_address 10.0.0.11
 crudini --set /etc/nova/nova.conf glance api_servers http://controller:9292
 crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
 crudini --set /etc/nova/nova.conf placement region_name RegionOne
